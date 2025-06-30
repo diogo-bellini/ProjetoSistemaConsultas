8113066a4d23a5d12e07a9f7c5d96fe611ac86ac
Diogo-Bellini <diogocvazbellini@icloud.com>
2025-06-30 00:06:17 -0300

    add: atualizar cadastro medico

--- src/main/java/com/carely/sistema_consultas/controller/IMedicoService.java
+++ src/main/java/com/carely/sistema_consultas/controller/IMedicoService.java
@@ -4,4 +4,5 @@ import com.carely.sistema_consultas.entity.Medico;
 
 public interface IMedicoService {
     public Medico carregarMedicoEmail(String email);
+    void atualizarPerfil(Medico medico, String senha);
 }
--- src/main/java/com/carely/sistema_consultas/controller/MedicoController.java
+++ src/main/java/com/carely/sistema_consultas/controller/MedicoController.java
@@ -1,11 +1,11 @@
 package com.carely.sistema_consultas.controller;
 
+import com.carely.sistema_consultas.entity.Medico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/medico")
@@ -18,6 +18,19 @@ public class MedicoController {
     public String home(Model model) {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         model.addAttribute("medico", medicoService.carregarMedicoEmail(email));
+        return "medico/home";
+    }
+
+    @GetMapping("/editar-perfil")
+    public String editarPerfil(Model model) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        model.addAttribute("medico", medicoService.carregarMedicoEmail(email));
+        return "medico/editar-perfil/editar-perfil";
+    }
+
+    @PostMapping("/editar-perfil")
+    public String editarPerfil(@ModelAttribute("medico") Medico medico, @RequestParam(required = false) String senha) {
+        medicoService.atualizarPerfil(medico, senha);
         return "medico/home";
     }
 }
