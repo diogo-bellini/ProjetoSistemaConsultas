0657d7b7dc93edbad9762846839c35d20a28196f
jpmrangel <joaopaulomr96@gmail.com>
2025-07-02 00:06:46 -0300

    fix: frequencia detalhes e confirmar do paciente

--- src/main/java/com/carely/sistema_consultas/controller/consultaPacienteController/IAgendamentoConsultaServiceConsultaPaciente.java
+++ src/main/java/com/carely/sistema_consultas/controller/consultaPacienteController/IAgendamentoConsultaServiceConsultaPaciente.java
@@ -9,6 +9,7 @@ import java.util.Set;
 
 public interface IAgendamentoConsultaServiceConsultaPaciente {
     public AgendamentoConsulta carregarAgendamentoConsulta(Long id);
+    public void confirmarAgendamento(AgendamentoConsulta agendamentoConsulta);
     public void cancelarAgendamento(AgendamentoConsulta agendamentoConsulta);
     public AgendamentoConsulta carregarAgendamentoConsultaComMedico(Long id);
     public Set<String> buscarHorariosIndisponiveis(Long medicoId);
